22813a60fd290b29a26dd272591c7d348d38815f
Tomas Bedrich <tbedrich@akamai.com>
2020-10-25 22:14:46 +0100

    Update cron intervals

--- cashier/config.py
+++ cashier/config.py
@@ -10,8 +10,8 @@ config.init("SENTRY_DSN", str, None)
 config.init("SENTRY_ENVIRONMENT", str, "production")
 
 # https://crontab.guru/
-config.init("TRANSACTION_WATCH_CRON", str, "0 19 * * *")  # daily at 19:00
-config.init("FLIGHT_WATCH_CRON", str, "0 20 1 * *")  # monthly on 1st at 20:00
+config.init("TRANSACTION_WATCH_CRON", str, "0 * * * *")  # each hour
+config.init("FLIGHT_WATCH_CRON", str, "0 20 * * *")  # every day at 20:00
 
 config.init("LIVENESS", Path, Path("/tmp/liveness"))
 config.init("LIVENESS_SLEEP", int, 10)  # seconds
